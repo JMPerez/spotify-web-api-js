68189c6fbfa095553f0bce111e00bd5b85e16738
JMPerez <jmperez1985@gmail.com>
2014-04-15 23:25:44 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,160 @@
-spotify-web-api-js
+Spotify Web API JS
 ==================
 
-A JS wrapper for the Spotify Web API
+This is a wrapper for the Spotify Web API. It includes helper functions to make requests for getting albums, artists, tracks, playlists and users information.
+
+The wrapper supports callback functions, as well as Promise (you can also use [a polyfill](https://github.com/jakearchibald/es6-promise), [Q](https://github.com/kriskowal/q) and [when](https://github.com/cujojs/when) if they are available.
+
+## Usage
+
+_Note: The following examples use Promises as the return object._
+
+First, instantiate the wrapper.
+
+```javascript
+var spotifyApi = new SpotifyAPIWrapper();
+```
+
+If you have an access token, you can set it doing:
+
+```javascript
+spotifyApi.setAccessToken('<here_your_access_token');
+```
+
+When you set an access token, it will be used for signing your requests. Note that an access token is not always necessary, unless you want to sign your requests or have access to data that a user has granted access to your app.
+
+Here you see how we can get basic information:
+
+
+The wrapper relies on [Promises](http://www.html5rocks.com/en/tutorials/es6/promises/). If the browser doesn't support them, you need to include a [polyfill](https://github.com/jakearchibald/es6-promise).
+
+
+
+```javascript
+spotifyApi.getAlbum('5U4W9E5WsYb2jUQWePT8Xm')
+  .then(function(result) {
+    console.log('Album information', result);
+  }, function(err) {
+    console.log(err);
+  });
+
+spotifyApi.getAlbums(['5U4W9E5WsYb2jUQWePT8Xm', '3KyVcddATClQKIdtaap4bV'])
+  .then(function(result) {
+    console.log('Albums information', result);
+  }, function(err) {
+    console.log(err);
+  });
+
+spotifyApi.getArtist('2hazSY4Ef3aB9ATXW7F5w3')
+  .then(function(result) {
+    console.log('Artist information', result);
+  }, function(err) {
+    console.log(err);
+  });
+
+spotifyApi.getArtists(['2hazSY4Ef3aB9ATXW7F5w3', '6J6yx1t3nwIDyPXk5xa7O8'])
+  .then(function(result) {
+    console.log('Artists information', result);
+  }, function(err) {
+    console.log(err);
+  });
+
+spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+  .then(function(result) {
+    console.log('Artist albums', result);
+  }, function(err) {
+    console.log(err);
+  });
+
+spotifyApi.search('Love')
+  .then(function(result) {
+    console.log('Search by "Love" (defaults to track results)', result);
+  }, function(err) {
+    console.log(err);
+  });
+```
+
+### Extra options
+You can pass extra options to some functions, such as the ones supporting pagination. These options will be sent as query parameters:
+
+```javascript
+// get Elvis' albums in range [20...29]
+spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', {limit: 10, offset: 20})
+  .then(function(result) {
+    console.log('Artist albums', result);
+  }, function(err) {
+    console.log(err);
+  });
+
+// search artists whose name contains 'Love'
+spotifyApi.search('Love', {type: 'artist'})
+  .then(function(result) {
+    console.log('Search artists by "Love"', result);
+  }, function(err) {
+    console.log(err);
+  });
+
+// search tracks whose artist's name contains 'Love'
+spotifyApi.search('artist:Love', {type: 'track'})
+  .then(function(result) {
+    console.log('Search tracks by "Love" in the artist name', result);
+  }, function(err) {
+    console.log(err);
+  });
+```
+
+### Nesting calls
+When you need to make multiple calls to get some dataset, you can take advantage of the Promises to get 
+
+```javascript
+// track detail information for album tracks
+spotifyApi.getAlbum('5U4W9E5WsYb2jUQWePT8Xm')
+  .then(function(result) {
+    return result.tracks.map(function(t) { return t.id; });
+  })
+  .then(function(trackIds) {
+    return spotifyApi.getTracks(trackIds);
+  })
+  .then(function(tracksInfo) {
+    console.log(tracksInfo);
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
+
+// album detail for the first 10 Elvis' albums
+spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', {limit: 10})
+  .then(function(result) {
+    return result.albums.map(function(a) { return a.id; });
+  })
+  .then(function(albums) {
+    return spotifyApi.getAlbums(albums);
+  }).then(function(result) {
+    console.log(result);
+  });
+```
+
+### Getting user's information
+In order to get user's information you will probably need to request an access token. Say for instance you want to get user's playlists. Once you get an access token, set it
+
+```javascript
+// get an access token
+...
+
+// set it in the wrapper
+var spotifyApi = new SpotifyAPIWrapper();
+spotifyApi.setAccessToken('<here_your_access_token');
+spotifyApiWithToken.getUserPlaylists('jmperezperez')
+  .then(function(result) {
+    console.log('User playlists', result);
+  }, function(err) {
+    console.log(err);
+  });
+
+spotifyApiWithToken.getUserPlaylist('jmperezperez', '4vHIKV7j4QcZwgzGQcZg1x')
+  .then(function(result) {
+    console.log('User playlist', result);
+  }, function(err) {
+    console.log(err);
+  });
+```
---
+++ spotify-web-api.js
@@ -0,0 +1,212 @@
+var SpotifyWebApi = (function() {
+
+  var _baseUri = 'https://api.spotify.com/v1';
+  var _accessToken = null;
+
+  var _promiseProvider = function(promiseFunction, callback) {
+    if (window.Promise) {
+      return new Promise(promiseFunction);
+    }
+    else {
+      var other = window.Q || window.when;
+      if (other) {
+        var deferred = other.defer();
+        promiseFunction(function(resolvedResult) {
+          deferred.resolve(resolvedResult);
+        }, function(rejectedResult) {
+          deferred.reject(rejectedResult);
+        });
+        return deferred.promise;
+      } else {
+        if (callback) {
+          return promiseFunction(function(resolvedResult) {
+            callback(null, resolvedResult);
+          }, function(rejectedResult) {
+            callback(rejectedResult, null);
+          });
+        }
+      }
+    }
+    return null;
+  };
+
+  var _checkParamsAndPerformRequest = function(requestData, options, callback) {
+    var opt = {};
+    var cb = null;
+
+    if (typeof options === 'object') {
+      opt = options;
+      cb = callback;
+    } else if (typeof options === 'function') {
+      cb = options;
+    }
+    _extend(requestData.params, opt);
+    return _performRequest(requestData, cb);
+  };
+
+  var _performRequest = function(requestData, callback) {
+    var promiseFunction = function(resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.open(requestData.type || 'GET',
+        _buildUrl(requestData.url, requestData.params),
+        true);
+      
+      if (_accessToken) {
+        req.setRequestHeader('Authorization', 'Bearer ' + _accessToken);
+      }
+
+      req.onload = function() {
+        if (req.status == 200) {
+          var data = JSON.parse(req.responseText);
+          if (resolve) {
+            resolve(data);
+          }
+          callback(null, data);
+        } else {
+          if (reject) {
+            reject(req);
+          }
+          callback(new Error(req), null);
+        }
+      };
+      req.send(null);
+    };
+
+    return _promiseProvider(promiseFunction, callback);
+  };
+
+  var _extend = function(params) {
+    var args = Array.prototype.slice.call(arguments);
+    var target = args[0];
+    var objects = args.slice(1);
+    target = target || {};
+    for (var i = 0; i < objects.length; i++) {
+      for (var j in objects[i]) {
+        if (!(j in target)) {
+          target[j] = objects[i][j];
+        }
+      }
+    }
+    return target;
+  };
+
+  var _buildUrl = function(url, parameters){
+    var qs = '';
+    for (var key in parameters) {
+      var value = parameters[key];
+      qs += encodeURIComponent(key) + '=' + encodeURIComponent(value) + '&';
+    }
+    if (qs.length > 0){
+      qs = qs.substring(0, qs.length - 1); //chop off last '&'
+      url = url + '?' + qs;
+    }
+    return url;
+  };
+
+  var Constr = function() {};
+
+  // public API — prototype
+  Constr.prototype = {
+    constructor: SpotifyWebApi
+  };
+
+  Constr.prototype.getGeneric = function(url, callback) {
+    var requestData = {
+      url: url
+    };
+    return _checkParamsAndPerformRequest(requestData, callback);
+  };
+
+  Constr.prototype.getMe = function(options, callback) {
+    var requestData = {
+      url: _baseUri + '/me'
+    };
+    return _checkParamsAndPerformRequest(requestData, options, callback);
+  };
+
+  Constr.prototype.getUser = function(userId, options, callback) {
+    var requestData = {
+      url: _baseUri + '/users/' + userId
+    };
+    return _checkParamsAndPerformRequest(requestData, options, callback);
+  };
+
+  Constr.prototype.getUserPlaylist = function(userId, playlistId, options, callback) {
+    var requestData = {
+      url: _baseUri + '/users/' + userId + '/playlists/' + playlistId
+    };
+    return _checkParamsAndPerformRequest(requestData, options, callback);
+  };
+
+  Constr.prototype.getUserPlaylists = function(userId, options, callback) {
+    var requestData = {
+      url: _baseUri + '/users/' + albumId + '/playlists'
+    };
+    return _checkParamsAndPerformRequest(requestData, options, callback);
+  };
+
+  Constr.prototype.getAlbum = function(albumId, options, callback) {
+    var requestData = {
+      url: _baseUri + '/albums/' + albumId
+    };
+    return _checkParamsAndPerformRequest(requestData, options, callback);
+  };
+
+  Constr.prototype.getAlbums = function(albumIds, options, callback) {
+    var requestData = {
+      url: _baseUri + '/albums/',
+      params: { ids: albumIds.join(',') }
+    };
+    return _checkParamsAndPerformRequest(requestData, options, callback);
+  };
+
+  Constr.prototype.getTrack = function(trackId, options, callback) {
+    var requestData = {};
+    requestData.url = _baseUri + '/tracks/' + trackId;
+    return _checkParamsAndPerformRequest(requestData, options, callback);
+  };
+
+  Constr.prototype.getTracks = function(trackIds, options, callback) {
+    var requestData = {
+      url: _baseUri + '/tracks/',
+      params: { ids: trackIds.join(',') }
+    };
+    return _checkParamsAndPerformRequest(requestData, options, callback);
+  };
+
+  Constr.prototype.getArtist = function(artistId, options, callback) {
+    var requestData = {
+      url: _baseUri + '/artists/' + artistId
+    };
+    return _checkParamsAndPerformRequest(requestData, options, callback);
+  };
+
+  Constr.prototype.getArtists = function(artistIds, options, callback) {
+    var requestData = {
+      url: _baseUri + '/artists/',
+      params: { ids: artistIds.join(',') }
+    };
+    return _checkParamsAndPerformRequest(requestData, options, callback);
+  };
+
+  Constr.prototype.getArtistAlbums = function(artistId, options, callback) {
+    var requestData = {
+      url: _baseUri + '/artists/' + artistId + '/albums'
+    };
+    return _checkParamsAndPerformRequest(requestData, options, callback);
+  };
+
+  Constr.prototype.search = function(query, options, callback) {
+    var requestData = {
+      url: _baseUri + '/search/',
+      params: { q: query }
+    };
+    return _checkParamsAndPerformRequest(requestData, options, callback);
+  };
+
+  Constr.prototype.setAccessToken = function(accessToken) {
+    _accessToken = accessToken;
+  };
+
+  return Constr;
+})();
